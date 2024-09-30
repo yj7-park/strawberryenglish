46bf019f523a4566423fd189be6fc24ea56dea93
Yongjun Park <shaneangel@naver.com>
2024-09-30 21:48:05 +0900

    회사 정보에 전화번호 추가

--- lib/widgets/company_info.dart
+++ lib/widgets/company_info.dart
@@ -21,7 +21,7 @@ class CompanyInfo extends StatelessWidget {
             children: [
               Text(
                 """
-딸기아카데미 | 519-55-00921 | 대표자 윤소명 | 이메일 strawberryacademy23@gmail.com
+딸기아카데미 | 519-55-00921 | 대표자 윤소명 | 이메일 strawberryacademy23@gmail.com | 전화번호 054-451-0722
 통신판매업 신고번호
 경북 구미시 고아읍 송평구길 62 나동 1402호
 
